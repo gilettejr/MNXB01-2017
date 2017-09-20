6d6d34a7218b27a36988b2e005615eb2f943df9e
Florido Paganelli <florido.paganelli@hep.lu.se>
2017-09-20 16:44:54 +0200

    added past year examples

---
+++ flopaganelli/Tutorial3bMaterial/bash/envtest.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# create an environment variable
+MYENV2="This is my second environment variable"
+
+# write the content of the environment vars to screen
+echo "Content of MYENV1: $MYENV1"
+echo "Content of MYENV2: $MYENV2"
---
+++ flopaganelli/Tutorial3bMaterial/bash/testif.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# testif.sh
+# run with: ./testif.sh arg1 arg2 arg3
+#
+# test that at least two arguments are passed to the script
+if  [[ $# -le 2 ]];  then
+    echo "Not enough arguments. Must be at least 3!";
+else
+    echo "More than 2 arguments. Good!";
+fi
