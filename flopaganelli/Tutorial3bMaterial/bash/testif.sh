0f0812c1d8382cae949a86c23d844ae53a9d0f0a
Florido Paganelli <florido.paganelli@hep.lu.se>
2017-09-22 10:49:55 +0200

    added exit command to testif.sh

--- flopaganelli/Tutorial3bMaterial/bash/testif.sh
+++ flopaganelli/Tutorial3bMaterial/bash/testif.sh
@@ -5,6 +5,8 @@
 # test that at least two arguments are passed to the script
 if  [[ $# -le 2 ]];  then
     echo "Not enough arguments. Must be at least 3!";
+    exit 1;
 else
     echo "More than 2 arguments. Good!";
+    exit 0;
 fi
